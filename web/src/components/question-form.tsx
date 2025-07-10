2289200bb528e9e744db093295a44cda0bac74be
Rafael Menegheti <rafaelmenegheti51@gmail.com>
2025-07-10 16:47:50 -0300

    feat: addind audio response

--- web/src/components/question-form.tsx
+++ web/src/components/question-form.tsx
@@ -18,6 +18,7 @@ import {
   FormMessage,
 } from '@/components/ui/form'
 import { Textarea } from '@/components/ui/textarea'
+import { useCreateQuestion } from '@/http/use-create-question'
 
 // Esquema de validação no mesmo arquivo conforme solicitado
 const createQuestionSchema = z.object({
@@ -35,6 +36,8 @@ interface QuestionFormProps {
 }
 
 export function QuestionForm({ roomId }: QuestionFormProps) {
+  const { mutateAsync: createQuestion } = useCreateQuestion(roomId)
+
   const form = useForm<CreateQuestionFormData>({
     resolver: zodResolver(createQuestionSchema),
     defaultValues: {
@@ -42,9 +45,10 @@ export function QuestionForm({ roomId }: QuestionFormProps) {
     },
   })
 
-  function handleCreateQuestion(data: CreateQuestionFormData) {
-    // biome-ignore lint/suspicious/noConsole: dev
-    console.log(data, roomId)
+  const { isSubmitting } = form.formState
+
+  async function handleCreateQuestion(data: CreateQuestionFormData) {
+    await createQuestion(data)
   }
 
   return (
@@ -72,6 +76,7 @@ export function QuestionForm({ roomId }: QuestionFormProps) {
                       className="min-h-[100px]"
                       placeholder="O que você gostaria de saber?"
                       {...field}
+                      disabled={isSubmitting}
                     />
                   </FormControl>
                   <FormMessage />
@@ -79,7 +84,9 @@ export function QuestionForm({ roomId }: QuestionFormProps) {
               )}
             />
 
-            <Button type="submit">Enviar pergunta</Button>
+            <Button disabled={isSubmitting} type="submit">
+              Enviar pergunta
+            </Button>
           </form>
         </Form>
       </CardContent>
